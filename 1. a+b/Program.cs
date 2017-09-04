bff6dacd8fd0d4159593b8a3a8d403804dfe4e95
Ilya Melnikov <ilya.melnikov@indusoft.ru>
2017-09-04 17:21:29 +0300

    2 tasks

---
+++ 1. a+b/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace _1.a_b
+{
+    class Program
+    {
+        private static int[] ReadFromFile()
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(@"input.txt");
+       
+            return file.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        }
+
+        private static void WriteToFile(string result)
+        {
+            System.IO.File.WriteAllText(@"output.txt", result);
+        }
+
+        static void Main(string[] args)
+        {
+            var input = ReadFromFile();
+            var result = input.Sum();
+            WriteToFile(result.ToString());
+        }
+    }
+}
---
+++ 2. a+squareb/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.a_squareb
+{
+    class Program
+    {
+        private static long[] ReadFromFile()
+        {
+            var file = new System.IO.StreamReader(@"input.txt");
+
+            return file.ReadLine().Split(' ').Select(long.Parse).ToArray();
+        }
+
+        private static void WriteToFile(string result)
+        {
+            System.IO.File.WriteAllText(@"output.txt", result);
+        }
+
+        static void Main(string[] args)
+        {
+            var input = ReadFromFile();
+            var result = input[0] + input[1] * input[1];
+            WriteToFile(result.ToString());
+        }
+    }
+}
