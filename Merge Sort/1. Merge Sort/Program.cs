e1215644b79123f8b9e09e07c7f601bc09b95af5
Ilya Melnikov <ilya.melnikov@indusoft.ru>
2018-09-21 15:18:18 +0300

    merge sort update

--- Merge Sort/1. Merge Sort/Program.cs
+++ Merge Sort/1. Merge Sort/Program.cs
@@ -1,14 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace _1.Merge_Sort
 {
     class Program
     {
-        static void Main(string[] args)
+        #region File operations
+        private static long[] ReadFromFile(out int arrayLength)
         {
+            var file = new System.IO.StreamReader(@"input.txt");
+
+            arrayLength = int.Parse(file.ReadLine() ?? throw new InvalidOperationException());
+
+            return file.ReadLine()?.Split(' ').Select(long.Parse).ToArray();
+        }
+
+        private static void WriteToFile(string result, string evidence)
+        {
+            System.IO.File.WriteAllText(@"output.txt", result + Environment.NewLine + evidence);
+        }
+        #endregion
+
+        private static long[] Array { get; set; }
+
+        private static void SortArrayByMergeMethod(out long[] evidence)
+        {
+            evidence = new long[Array.Length];
+            evidence[0] = 1;
+
+            for (var i = 1; i < Array.Length; i++)
+            {
+                var j = i - 1;
+                while (j >= 0 && Array[j] > Array[j + 1])
+                {
+                    Swap(j + 1, j);
+                    j--;
+                }
+                evidence[i] = j + 2;
+            }
+        }
+
+        private static void Swap(long i, long j)
+        {
+            var temp = Array[i];
+            Array[i] = Array[j];
+            Array[j] = temp;
+        }
+
+        static void Main()
+        {
+            Array = ReadFromFile(out _);
+
+            SortArrayByMergeMethod(out var evidence);
+
+            WriteToFile(string.Join(" ", evidence), string.Join(" ", Array));
         }
     }
 }
